3fbe869fb4279560ff5eca48846361069a6987d7
2PT-2 <118617121+zieduvija@users.noreply.github.com>
2023-04-19 12:22:05 +0300

    Iveidots UI un sagatavots skripta pievienošanai

---
+++ Assets/Skripti/vards.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vards : MonoBehaviour {
+
+	public int kadruSkaits;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Tu esi " + kadruSkaits + ". kadrā!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Kadrs: " + kadruSkaits);
+		
+	}
+}
